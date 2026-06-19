ad9567d1b9ef761ca73f55403e8354af34f0d95e
CI Pipeline Runner <ci-runner@example.com>
2026-06-18 19:32:31 -0500

    Primer commit - patrones de diseño

---
+++ src/main/java/org/example/Enunciado1/PoolConnection.java
@@ -0,0 +1,8 @@
+package org.example.Enunciado1;
+//PATRON; SINGLETON
+// Ejercicio ;
+//En tu aplicación hay un pool de conexiones a base de datos costoso de crear.
+// Varios módulos lo necesitan y todos deben trabajar contra el mismo recurso compartido. Diséñalo.
+public class PoolConnection {
+}
+https://github.com/Miguels5847/alvarez_software_patterns_design.git
